e24955bd599ecc8f7adf1b6d51817e85b1a6cbd8
laxcyma <144672000+laxcyma@users.noreply.github.com>
2023-10-09 18:00:22 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from razorapi import views
-urlpatterns = [
-    path('',views.payment_panel, name='payment_panel'),
-    path('payment_status/',views.payment_status, name='payment_status'),
-]
