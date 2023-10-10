dd6c54e1e3c4f0cceafbb7ebf3480e9405855af1
laxcyma <144672000+laxcyma@users.noreply.github.com>
2023-10-10 10:49:24 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('config/', views.config, name='razor-config'),
-    path('callback/', views.callback, name='razor-callback')]
+    path('config/',views.payment_panel, name='payment_panel'),
+    path('payment_status/',views.payment_status, name='payment_status'),
+]
