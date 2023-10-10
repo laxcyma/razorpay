c9a31967bbe77ab781074c782b46ad88b39edcf9
laxcyma <144672000+laxcyma@users.noreply.github.com>
2023-10-10 10:51:52 +0530

    Update razorpay.py

--- razorpay.py
+++ razorpay.py
@@ -28,9 +28,9 @@ def payment_panel(request):
                 'key_id':'rzp_test_5QkTsF3niAwffV'
 
             }
-            return render(request, 'index.html', context)
+            return render(request, 'templates/index.html', context)
     # Create a Razorpay Order
-    return render(request, 'index.html')
+    return render(request, 'templates/index.html')
     
 # example =  {'id': 'order_Mjd5DMLJlcw7GZ', 'entity': 'order', 'amount': 10000, 'amount_paid': 0, 'amount_due': 10000, 'currency': 'INR', 'receipt': None, 'offer_id': None, 'status': 'created', 'attempts': 0, 'notes': [], 'created_at': 1696313248}
 
